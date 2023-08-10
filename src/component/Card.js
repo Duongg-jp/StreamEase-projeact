6d9272b659240af94bd82f13d89d7be37bdb5a93
PMinhDucz <phamminhduc141@gmail.com>
2023-08-10 20:53:48 +0700

    init shop page

---
+++ src/component/Card.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Card.css'
+export default function Card(props) {
+    const product = {}
+    return (
+        <div className="card-container">
+            <div className="image-container">
+                <img src={product.imageUrl} alt="picture" />
+            </div>
+            <div className='card-content'>
+                <div className='card-title'>
+                    <h3>{product.title}</h3>
+                </div>
+                <div className='card-body'>
+                    <p>{product.description}
+                    </p>
+                </div>
+            </div>
+
+            <div className='btn'>
+                <button >
+                    <div>
+                        <a>add to card</a>
+                        <a>{product.price}</a>
+                    </div>
+                </button>
+            </div>
+        </div>
+    );
+};
---
+++ src/page/shop.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react'
+import dummyData from '../dummyData'
+
+export default function ShoppPage (){
+    const [listProducts, setListProducts] = useState();
+
+    return <div></div>
+}
