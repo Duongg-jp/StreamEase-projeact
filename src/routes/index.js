ed29253f5907a5884b49771390bd1131996be294
Duongg-jp <itachi.hp9x@gmail.com>
2023-08-10 21:23:25 +0700

    add css

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../page/home-page";
 import Contact from "../components/contact";
 import Feedback from "../components/feedback";
+import Error from "../page/404";
 
 export default function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
       <Route path="/" element={<HomePage />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/feedback" element={<Feedback />} />
+      <Route path="/*" element={<Error /> }></Route>
     </Routes>
   );
 }
