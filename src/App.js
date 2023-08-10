e4ac446b2729b82ef80d866fc2beb49bc94b7ca8
PMinhDucz <phamminhduc141@gmail.com>
2023-08-10 20:48:49 +0700

    saving card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-
+import  Card  from "./component/Card"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>gg</h1>
-      </header>
+      <Card 
+      />
     </div>
+    
+
+
   );
 }
 
